c94a581206cc0d6cef8a82672314bef9369f2862
addleton <michaeladdleton1@gmail.com>
2023-11-24 09:35:37 +0000

    completes task 16, adding express routers

--- app.js
+++ app.js
@@ -1,46 +1,12 @@
 const express = require("express");
 const app = express();
-const {
-    getArticles,
-    getArticleById,
-    patchArticles,
-} = require("./controllers/articles.controllers");
-const { getApi } = require("./controllers/app.controllers");
-const { getTopics } = require("./controllers/topics.controllers");
-const {
-    postCommentByArticle,
-    getComments,
-    removeComments,
-} = require("./controllers/comments.controllers");
-const {
-    handlePsqlErrors,
-    handleCustomErrors,
-    handleInvalidUrl,
-} = require("./errors");
-const { getUsers } = require("./controllers/users.controllers");
-
-
-
-
-
-
+const { handlePsqlErrors, handleCustomErrors } = require("./errors");
+const apiRouter = require("./routers/api-router");
 
 app.use(express.json());
-
-app.get("/api", getApi);
-app.get("/api/topics", getTopics);
-app.get("/api/articles", getArticles);
-app.get("/api/articles/:article_id", getArticleById);
-app.get("/api/articles/:article_id/comments", getComments);
-app.get("/api/users", getUsers);
-
-app.post("/api/articles/:article_id/comments", postCommentByArticle);
-app.patch("/api/articles/:article_id", patchArticles);
-app.delete("/api/comments/:comment_id", removeComments);
-app.all("*", handleInvalidUrl);
+app.use("/api", apiRouter);
 
 app.use(handlePsqlErrors);
 app.use(handleCustomErrors);
-app.use(handleInvalidUrl);
 
 module.exports = app;
--- controllers/app.controllers.js
+++
@@ -1,7 +0,0 @@
-const { selectApi } = require("../models/app.models");
-
-exports.getApi = (req, res, next) => {
-    selectApi().then((result) => {
-        res.status(200).send(result);
-    });
-};
